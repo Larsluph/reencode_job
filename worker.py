f6521906cd0e298b9e2457dbb392e6df8a2d675b
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-06-01 19:02:26 +0200

    Resolve lint issues

--- worker.py
+++ worker.py
@@ -1,7 +1,7 @@
 import logging
 from os import rename
 from pathlib import Path
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, TimeoutExpired
 
 from app import App
 from command_generator import generate_ffmpeg_command
@@ -25,15 +25,20 @@ def calc_ratio(isize, osize):
 
 
 class Worker:
+    """Worker class that processes a file"""
     app: App
     i: int
     max_i: int
     input_filename: Path
+    output_filename: Path
+    process: Popen = None
 
-    def __init__(self, app, i: int, input_filename: Path):
+    def __init__(self, app: App, i: int, input_filename: Path):
         self.app = app
         self.i = i
         self.input_filename = input_filename
+        self.output_filename = Path(self.input_filename.parent,
+                               f"{self.input_filename.stem}_reencoded.mp4")
 
     def work(self) -> bool:
         logger.info('[%d/%d] Processing "%s"', self.i, len(self.app.files), self.input_filename)
@@ -44,12 +49,13 @@ class Worker:
             return True
 
         errors = check_file(file_metadata)
-        output_filename = Path(self.input_filename.parent,
-                               f"{self.input_filename.stem}_reencoded.mp4")
-        if output_filename.exists():
-            logger.warning('Output file "%s" already exists, skipping', output_filename)
+        if self.output_filename.exists():
+            logger.warning('Output file "%s" already exists, skipping', self.output_filename)
             return True
-        cmd = generate_ffmpeg_command(self.input_filename, output_filename, file_metadata, errors)
+        cmd = generate_ffmpeg_command(self.input_filename,
+                                      self.output_filename,
+                                      file_metadata,
+                                      errors)
 
         logger.debug(file_metadata)
         logger.info(errors)
@@ -60,18 +66,26 @@ class Worker:
             return True
 
         if not self.app.is_dry_run_enabled:
-            with Popen(cmd, stdout=PIPE, stderr=STDOUT, universal_newlines=True) as process:
-                for line in process.stdout:
-                    logger.debug(line.rstrip())
-                    if self.app.is_interrupted:
-                        process.terminate()
+            with Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True) as self.process:
+                while not self.app.is_interrupted and self.process.poll() is not None:
+                    try:
+                        outs, errs = self.process.communicate(timeout=1)
+                        for line in outs.split():
+                            logger.debug(line.rstrip())
+                        for line in errs.split():
+                            logger.error(line.rstrip())
+                    except TimeoutExpired:
+                        pass
 
-                if process.wait() != 0:
+                    if self.app.is_interrupted:
+                        self.process.terminate()
+
+                if self.process.wait() != 0:
                     logger.error('Failed to process "%s": return code was %d',
-                                self.input_filename, process.returncode)
+                                self.input_filename, self.process.returncode)
                     if self.app.is_clean_on_error_enabled:
-                        logger.info('Removing failed "%s"', output_filename)
-                        output_filename.unlink()
+                        logger.info('Removing failed "%s"', self.output_filename)
+                        self.output_filename.unlink()
 
                     if self.app.is_interrupted:
                         logger.info('Interrupted')
@@ -79,18 +93,27 @@ class Worker:
                     return True
 
             in_size = file_metadata.file_size
-            out_size = output_filename.stat().st_size
+            out_size = self.output_filename.stat().st_size
 
             logger.info('%s -> %s (ratio: %.2fx) (saved: %s)',
                         format_bytes(in_size), format_bytes(out_size),
                         calc_ratio(in_size, out_size), format_bytes(in_size - out_size))
 
+        return True
+
+    def terminate_job(self):
+        if self.process is not None:
+            self.process.terminate()
+            return self.process.poll()
+        return None
+
+    def _cleanup(self):
         if self.app.is_replace_enabled:
             logger.info('Replacing "%s"', self.input_filename)
             # Replace the original file but keep the new extension
-            new_name = Path(self.input_filename).with_suffix(output_filename.suffix)
+            new_name = Path(self.input_filename).with_suffix(self.output_filename.suffix)
             if not self.app.is_dry_run_enabled:
-                rename(output_filename, new_name)
+                rename(self.output_filename, new_name)
                 if self.input_filename != new_name:
                     self.input_filename.unlink()
         elif self.app.is_remove_enabled:
@@ -98,4 +121,3 @@ class Worker:
             # Remove the original file
             if not self.app.is_dry_run_enabled:
                 self.input_filename.unlink()
-        return True
